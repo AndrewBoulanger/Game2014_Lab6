a98cd6b93c8903a7b5903c39ca7dfc4bf5d2d402
Andrew Boulanger <andrew_boulanger14@hotmail.com>
2021-11-12 19:40:20 -0500

    Revert "added movement animation and air control"
    This reverts commit d9d33989207a7a7deb989ea47824e1d3667261a7.

--- Assets/[Scripts]/PlayerBehaviour.cs
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -2,20 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerBehaviour : MonoBehaviour
 {
     [Header("Movement")]
     public float horizontalForce;
     public float verticalForce;
-    [Range(0.1f, 0.99f)]
-    public float airControlFactor = 0.5f;
     public bool isGrounded;
 
     private Rigidbody2D rigidbody;
-    private Animator animator;
-
-    public PlayerAnimationStates state;
 
     public Transform groundOrigin;
     public float groundRadius;
@@ -25,7 +19,7 @@ public class PlayerBehaviour : MonoBehaviour
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
@@ -38,21 +32,16 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void Move()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        if (x != 0)
-        {
-            FlipAnimation(x);
-        }
-
-        if (isGrounded)
+        if(isGrounded)
         {
 
+            float x = Input.GetAxisRaw("Horizontal");
             float y = Input.GetAxisRaw("Vertical");
             float jump = Input.GetAxisRaw("Jump");
 
-            //set animation state based on x input
-            state = (x == 0) ? PlayerAnimationStates.Idle : PlayerAnimationStates.Running;
-          
+            //check sprite flip
+            if(x != 0)
+                FlipAnimation(x);
 
             Vector2 worldTouch = new Vector2();
             foreach (var touch in Input.touches)
@@ -63,22 +52,21 @@ public class PlayerBehaviour : MonoBehaviour
             float horizontalMoveForce = x * horizontalForce;
             float jumpMoveForce = jump * verticalForce;
 
-            AddForce(horizontalMoveForce, jumpMoveForce);
-        }
-        else
-        {
-            state = PlayerAnimationStates.Jumping;
+            float mass = rigidbody.mass * rigidbody.gravityScale;
 
-            float horizontalMoveForce = x * horizontalForce * airControlFactor;
-            AddForce(horizontalMoveForce, 0.0f);
-        }
+            rigidbody.AddForce(new Vector2(horizontalMoveForce, jumpMoveForce) * mass);
 
-        animator.SetInteger("AnimationState", (int)state);
+            Vector2 velocity = rigidbody.velocity;
+            velocity.x *= 0.99f;
+            rigidbody.velocity = velocity;
+            //
+
+        }
     }
 
     private void CheckIfGrounded()
     {
-        RaycastHit2D hit = Physics2D.CircleCast(groundOrigin.position, groundRadius, Vector2.down, groundRadius, groundLayerMask);
+        RaycastHit2D hit = Physics2D.CircleCast(groundOrigin.position, groundRadius, Vector2.down, float.Epsilon, groundLayerMask);
 
         isGrounded = (hit) ? true: false;
     }
@@ -94,17 +82,7 @@ public class PlayerBehaviour : MonoBehaviour
     }
 
 
-    private void AddForce(float xForce, float yForce)
-    {
-        float mass = rigidbody.mass * rigidbody.gravityScale;
 
-        rigidbody.AddForce(new Vector2(xForce, yForce) * mass);
-
-        Vector2 velocity = rigidbody.velocity;
-        velocity.x *= 0.9f;
-        velocity.y *= 0.99f;
-        rigidbody.velocity = velocity;
-    }
 
 
     private void OnDrawGizmos()
